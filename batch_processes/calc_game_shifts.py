552b71ae56f62bd9f543c4bb920ee09f038a8cc6
mcbarlowe <mcbarlowe@gmail.com>
2019-10-09 23:30:16 -0400

    rapm calculations for single year is finished along with shift parsing scripts as well

--- batch_processes/calc_game_shifts.py
+++ batch_processes/calc_game_shifts.py
@@ -7,7 +7,157 @@ import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
 
-def calc_possession(game_df):
+def parse_possessions(poss_list):
+    '''
+    a function to parse each possession and create one row for offense team
+    and defense team
+
+    Inputs:
+    poss_list   - list of dataframes each one representing one possession
+
+    Outputs:
+    parsed_list  - list of dataframes where each list inside represents the players on
+                   off and def and points score for each possession
+    '''
+    parsed_list = []
+
+    for df in poss_list:
+        if df.loc[df.index[-1], 'event_type_de'] in ['rebound', 'turnover']:
+            if df.loc[df.index[-1], 'event_type_de'] == 'turnover':
+                if df.loc[df.index[-1], 'event_team'] == df.loc[df.index[-1], 'home_team_abbrev']:
+                    row_df = pd.concat([df.loc[df.index[-1], 'home_player_1':'away_player_5_id'],
+                                        df.loc[df.index[-1], ['points_made_y', 'home_team_abbrev',
+                                                              'event_team', 'away_team_abbrev',
+                                                              'home_team_id', 'away_team_id', 'game_id',
+                                                              'game_date', 'season']]])
+
+                    parsed_list.append(pd.DataFrame([list(row_df)], columns=['off_player_1', 'off_player_1_id',
+                                                                           'off_player_2', 'off_player_2_id',
+                                                                           'off_player_3', 'off_player_3_id',
+                                                                           'off_player_4', 'off_player_4_id',
+                                                                           'off_player_5', 'off_player_5_id',
+                                                                           'def_player_1', 'def_player_1_id',
+                                                                           'def_player_2', 'def_player_2_id',
+                                                                           'def_player_3', 'def_player_3_id',
+                                                                           'def_player_4', 'def_player_4_id',
+                                                                           'def_player_5', 'def_player_5_id',
+                                                                           'points_made', 'home_team_abbrev',
+                                                                           'event_team_abbrev', 'away_team_abbrev',
+                                                                           'home_team_id', 'away_team_id', 'game_id',
+                                                                           'game_date', 'season']))
+                elif df.loc[df.index[-1], 'event_team'] == df.loc[df.index[-1], 'away_team_abbrev']:
+                    row_df = pd.concat([df.loc[df.index[-1], 'home_player_1':'away_player_5_id'],
+                                        df.loc[df.index[-1], ['points_made_y', 'home_team_abbrev',
+                                                              'event_team', 'away_team_abbrev',
+                                                              'home_team_id', 'away_team_id', 'game_id',
+                                                              'game_date', 'season']]])
+
+                    parsed_list.append(pd.DataFrame([list(row_df)], columns=['def_player_1', 'def_player_1_id',
+                                                                           'def_player_2', 'def_player_2_id',
+                                                                           'def_player_3', 'def_player_3_id',
+                                                                           'def_player_4', 'def_player_4_id',
+                                                                           'def_player_5', 'def_player_5_id',
+                                                                           'off_player_1', 'off_player_1_id',
+                                                                           'off_player_2', 'off_player_2_id',
+                                                                           'off_player_3', 'off_player_3_id',
+                                                                           'off_player_4', 'off_player_4_id',
+                                                                           'off_player_5', 'off_player_5_id',
+                                                                           'points_made', 'home_team_abbrev',
+                                                                           'event_team_abbrev', 'away_team_abbrev',
+                                                                           'home_team_id', 'away_team_id', 'game_id',
+                                                                           'game_date', 'season']))
+            if df.loc[df.index[-1], 'event_type_de'] == 'rebound':
+                if df.loc[df.index[-1], 'event_team'] == df.loc[df.index[-1], 'away_team_abbrev']:
+                    row_df = pd.concat([df.loc[df.index[-1], 'home_player_1':'away_player_5_id'],
+                                        df.loc[df.index[-1], ['points_made_y', 'home_team_abbrev',
+                                                              'event_team', 'away_team_abbrev',
+                                                              'home_team_id', 'away_team_id', 'game_id',
+                                                              'game_date', 'season']]])
+
+                    parsed_list.append(pd.DataFrame([list(row_df)], columns=['off_player_1', 'off_player_1_id',
+                                                                           'off_player_2', 'off_player_2_id',
+                                                                           'off_player_3', 'off_player_3_id',
+                                                                           'off_player_4', 'off_player_4_id',
+                                                                           'off_player_5', 'off_player_5_id',
+                                                                           'def_player_1', 'def_player_1_id',
+                                                                           'def_player_2', 'def_player_2_id',
+                                                                           'def_player_3', 'def_player_3_id',
+                                                                           'def_player_4', 'def_player_4_id',
+                                                                           'def_player_5', 'def_player_5_id',
+                                                                           'points_made', 'home_team_abbrev',
+                                                                           'event_team_abbrev', 'away_team_abbrev',
+                                                                           'home_team_id', 'away_team_id', 'game_id',
+                                                                           'game_date', 'season']))
+
+                elif df.loc[df.index[-1], 'event_team'] == df.loc[df.index[-1], 'home_team_abbrev']:
+                    row_df = pd.concat([df.loc[df.index[-1], 'home_player_1':'away_player_5_id'],
+                                        df.loc[df.index[-1], ['points_made_y', 'home_team_abbrev',
+                                                              'event_team', 'away_team_abbrev',
+                                                              'home_team_id', 'away_team_id', 'game_id',
+                                                              'game_date', 'season']]])
+
+                    parsed_list.append(pd.DataFrame([list(row_df)], columns=['def_player_1', 'def_player_1_id',
+                                                                           'def_player_2', 'def_player_2_id',
+                                                                           'def_player_3', 'def_player_3_id',
+                                                                           'def_player_4', 'def_player_4_id',
+                                                                           'def_player_5', 'def_player_5_id',
+                                                                           'off_player_1', 'off_player_1_id',
+                                                                           'off_player_2', 'off_player_2_id',
+                                                                           'off_player_3', 'off_player_3_id',
+                                                                           'off_player_4', 'off_player_4_id',
+                                                                           'off_player_5', 'off_player_5_id',
+                                                                           'points_made', 'home_team_abbrev',
+                                                                           'event_team_abbrev', 'away_team_abbrev',
+                                                                           'home_team_id', 'away_team_id', 'game_id',
+                                                                           'game_date', 'season']))
+
+        elif df.loc[df.index[-1], 'event_type_de'] in ['shot', 'free-throw']:
+            if df.loc[df.index[-1], 'event_team'] == df.loc[df.index[-1], 'home_team_abbrev']:
+                row_df = pd.concat([df.loc[df.index[-1], 'home_player_1':'away_player_5_id'],
+                                    df.loc[df.index[-1], ['points_made_y', 'home_team_abbrev',
+                                                          'event_team', 'away_team_abbrev',
+                                                          'home_team_id', 'away_team_id', 'game_id',
+                                                          'game_date', 'season']]])
+
+                parsed_list.append(pd.DataFrame([list(row_df)], columns=['off_player_1', 'off_player_1_id',
+                                                                       'off_player_2', 'off_player_2_id',
+                                                                       'off_player_3', 'off_player_3_id',
+                                                                       'off_player_4', 'off_player_4_id',
+                                                                       'off_player_5', 'off_player_5_id',
+                                                                       'def_player_1', 'def_player_1_id',
+                                                                       'def_player_2', 'def_player_2_id',
+                                                                       'def_player_3', 'def_player_3_id',
+                                                                       'def_player_4', 'def_player_4_id',
+                                                                       'def_player_5', 'def_player_5_id',
+                                                                       'points_made', 'home_team_abbrev',
+                                                                       'event_team_abbrev', 'away_team_abbrev',
+                                                                       'home_team_id', 'away_team_id', 'game_id',
+                                                                       'game_date', 'season']))
+            elif df.loc[df.index[-1], 'event_team'] == df.loc[df.index[-1], 'away_team_abbrev']:
+                row_df = pd.concat([df.loc[df.index[-1], 'home_player_1':'away_player_5_id'],
+                                    df.loc[df.index[-1], ['points_made_y', 'home_team_abbrev',
+                                                          'event_team', 'away_team_abbrev',
+                                                          'home_team_id', 'away_team_id', 'game_id',
+                                                          'game_date', 'season']]])
+
+                parsed_list.append(pd.DataFrame([list(row_df)], columns=['def_player_1', 'def_player_1_id',
+                                                                       'def_player_2', 'def_player_2_id',
+                                                                       'def_player_3', 'def_player_3_id',
+                                                                       'def_player_4', 'def_player_4_id',
+                                                                       'def_player_5', 'def_player_5_id',
+                                                                       'off_player_1', 'off_player_1_id',
+                                                                       'off_player_2', 'off_player_2_id',
+                                                                       'off_player_3', 'off_player_3_id',
+                                                                       'off_player_4', 'off_player_4_id',
+                                                                       'off_player_5', 'off_player_5_id',
+                                                                       'points_made', 'home_team_abbrev',
+                                                                       'event_team_abbrev', 'away_team_abbrev',
+                                                                       'home_team_id', 'away_team_id', 'game_id',
+                                                                       'game_date', 'season']))
+
+    return parsed_list
+
+def calc_possessions(game_df):
     '''
     funciton to calculate possesion numbers for both team and players
     and insert into possesion tables. This will only be used for pbp that doesn't
@@ -146,14 +296,29 @@ def extract_shifts(pbp_df, engine):
 
 def main():
     engine = create_engine(os.environ['NBA_CONNECT_DEV'])
-    seasons = [2016, 2017, 2018]
+    seasons = [2019, 2018, 2017]
     for season in seasons:
+        print(f"Processing season: {season}")
         pbp_df = pd.read_sql_query(f'select * from nba.pbp where season = {season};', engine)\
-            .sort_values(by=['game_id', 'seconds_elapsed', 'eventnum']).reset_index()
+            .sort_values(by=['game_id', 'seconds_elapsed', 'eventnum']).reset_index(drop=True)
+        pbp_df = calc_possessions(pbp_df)
         points_by_second = pbp_df.groupby(['game_id', 'seconds_elapsed'])['points_made'].sum().reset_index()
-        for game in range(int(f'2{str(season)[2:]}00001'), int(f'2{str(season)[2:]}01231')):
-            pbp_df = pd.read_sql_query(f'select * from nba.pbp where game_id = {game};', engine).sort_values(by=['seconds_elapsed', 'eventnum']).reset_index()
-            pbp_df = calc_possessions(pbp_df)
+        for game in range(int(f'2{str(season-1)[2:]}00001'), int(f'2{str(season-1)[2:]}01231')):
+            print(f'Processing game: {game}')
+            game_df = pbp_df[pbp_df.game_id == game].reset_index(drop=True)
+            poss_index = game_df[(game_df.home_possession == 1) | (game_df.away_possession == 1)].index
+            shift_dfs = []
+            past_index = 0
+            for i in poss_index:
+                shift_dfs.append(game_df.iloc[past_index+1: i+1, :].reset_index())
+                past_index = i
+            possession = [x.merge(points_by_second, on=['game_id', 'seconds_elapsed']) for x in shift_dfs]
+            print(f"Combining {game} possessions into one dataframe and inserting into database")
+            poss_df = pd.concat(parse_possessions(possession))
+            poss_df.to_sql('rapm_shifts', engine, schema='nba',
+                           if_exists='append', index=False, method='multi')
+
+
 
     pass
 if __name__ == '__main__':
--- batch_processes/rapm_calculation.py
+++ batch_processes/rapm_calculation.py
@@ -19,7 +19,7 @@ def map_players(row_in, players):
     p9 = row_in[8]
     p10 = row_in[9]
 
-    rowOut = np.zeros([len(players) * 2])
+    rowOut = np.zeros([(len(players) * 2) + 1])
 
     rowOut[players.index(p1)] = 1
     rowOut[players.index(p2)] = 1
@@ -33,6 +33,8 @@ def map_players(row_in, players):
     rowOut[players.index(p9) + len(players)] = -1
     rowOut[players.index(p10) + len(players)] = -1
 
+    rowOut[len(players) + 1] = row_in[10]
+
     return rowOut
 
 def one_year_rapm_calc(season, sa_engine):
@@ -42,12 +44,12 @@ def one_year_rapm_calc(season, sa_engine):
     '''
 
     # pull shifts from table
-    #shifts_df = pd.read_sql_query(f'select * from nba.rapm_shifts where season = {season};', sa_engine)
+    shifts_df = pd.read_sql_query(f'select * from nba.rapm_shifts where season = {season};', sa_engine)
 
     #shifts_df = shifts_df[shifts_df.possessions != 0]
 
     # pull out unique player ids
-    shifts_df = pd.read_csv('possessions_data.csv')
+    #shifts_df = pd.read_csv('possessions_data.csv')
     players = list(set(list(shifts_df['off_player_1_id'].unique()) +
                        list(shifts_df['off_player_2_id'].unique()) +
                        list(shifts_df['off_player_3_id'].unique()) +
@@ -61,57 +63,70 @@ def one_year_rapm_calc(season, sa_engine):
 
     players.sort()
     shifts_df['points_per_100_poss'] = shifts_df['points_made'] * 100
+    shifts_df['possessions'] = 1
+    shifts_df['is_home'] = np.where(shifts_df['home_team_abbrev'] == shifts_df['event_team_abbrev'], 1, 0)
     train_x = shifts_df.as_matrix(columns=['off_player_1_id', 'off_player_2_id',
                                                  'off_player_3_id', 'off_player_4_id', 'off_player_5_id',
                                                  'def_player_1_id', 'def_player_2_id',
-                                                 'def_player_3_id', 'def_player_4_id', 'def_player_5_id'])
+                                                 'def_player_3_id', 'def_player_4_id', 'def_player_5_id',
+                                                 'is_home'])
 
     train_x = np.apply_along_axis(map_players, 1, train_x, players)
     train_y = shifts_df.as_matrix(['points_per_100_poss'])
-    #possessions = shifts_df['possessions']
+    possessions = shifts_df['possessions']
 
     lambdas_rapm = [.01, .05, .1 ]
     alphas = [lambda_to_alpha(l, train_x.shape[0]) for l in lambdas_rapm]
     clf = RidgeCV(alphas=alphas, cv=5, fit_intercept=True, normalize=False)
-    model = clf.fit(train_x, train_y )
+    model = clf.fit(train_x, train_y, sample_weight=possessions )
     player_arr = np.transpose(np.array(players).reshape(1, len(players)))
 
     # extract our coefficients into the offensive and defensive parts
     coef_offensive_array = np.transpose(model.coef_[:, 0:len(players)])
-    coef_defensive_array = np.transpose(model.coef_[:, len(players):])
+    coef_defensive_array = np.transpose(model.coef_[:, len(players):-1])
 
     # concatenate the offensive and defensive values with the playey ids into a mx3 matrix
+    print(player_arr.shape)
+    print(coef_offensive_array.shape)
+    print(coef_defensive_array.shape)
     player_id_with_coef = np.concatenate([player_arr, coef_offensive_array, coef_defensive_array], axis=1)
     # build a dataframe from our matrix
     players_coef = pd.DataFrame(player_id_with_coef)
     intercept = model.intercept_
     name = 'rapm'
   # apply new column names
-    players_coef.columns = ['player_id', '{0}__Off'.format(name), '{0}__Def'.format(name)]
+    players_coef.columns = ['player_id', '{0}_off'.format(name), '{0}_def'.format(name)]
     # Add the offesnive and defensive components together (we should really be weighing this to the number of offensive and defensive possession played as they are often not equal).
-    players_coef[name] = players_coef['{0}__Off'.format(name)] + players_coef['{0}__Def'.format(name)]
+    players_coef[name] = players_coef['{0}_off'.format(name)] + players_coef['{0}_def'.format(name)]
 
     # rank the values
-    players_coef['{0}_Rank'.format(name)] = players_coef[name].rank(ascending=False)
-    players_coef['{0}__Off_Rank'.format(name)] = players_coef['{0}__Off'.format(name)].rank(ascending=False)
-    players_coef['{0}__Def_Rank'.format(name)] = players_coef['{0}__Def'.format(name)].rank(ascending=False)
+    players_coef['{0}_rank'.format(name)] = players_coef[name].rank(ascending=False)
+    players_coef['{0}_off_Rank'.format(name)] = players_coef['{0}_off'.format(name)].rank(ascending=False)
+    players_coef['{0}_def_Rank'.format(name)] = players_coef['{0}_def'.format(name)].rank(ascending=False)
 
     # add the intercept for reference
     players_coef['{0}__intercept'.format(name)] = intercept[0]
 
     print(f'This is the intercept of the model: {intercept}')
     print(players_coef.head())
-    #player_df = pd.read_sql_query(f'select * from nba.player_details;', sa_engine)
+    player_df = pd.read_sql_query(f'select * from nba.player_details;', sa_engine)
 
-    #results_df = players_coef.merge(player_df[['player_id', 'display_first_last']], on='player_id')
-    #results_df.to_csv('rapm_results.csv')
-    players_coef.to_csv('rapm_results.csv')
+    results_df = players_coef.merge(player_df[['player_id', 'display_first_last']], on='player_id')
+    results_df = np.round(results_df, decimals=2)
+    results_df.to_csv('rapm_results.csv')
+    results_df['season'] = season
+    results_df = results_df.rename(columns={'display_first_last': 'player_name'})
+    results_df.to_sql('player_single_year_rapm', sa_engine, schema='nba',
+                       if_exists='append', index=False, method='multi')
+
+    #players_coef.to_csv('rapm_results.csv')
 
 
 def main():
-    season = 2019
+    seasons = [2019, 2018, 2017]
     engine = create_engine(os.environ['NBA_CONNECT_DEV'])
-    one_year_rapm_calc(season, engine)
+    for season in seasons:
+        one_year_rapm_calc(season, engine)
 
 if __name__ == '__main__':
     main()
